9f0df25d168e146c93a0384541fbc5d1f48f1db5
AysunHasanova <aysunhsnva@gmail.com>
2023-06-05 22:10:08 +0400

    Startup section is completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,15 @@
+let nums = document.querySelectorAll(".num");
+let interval = 3000;
 
-
+nums.forEach((num) => {
+  let startNum = 0;
+  let endNum = parseInt(num.getAttribute("data-val"));
+  let duration = Math.floor(interval / endNum);
+  let counter = setInterval(function () {
+    startNum += 20;
+    num.textContent = startNum;
+    if (startNum == endNum) {
+      clearInterval(counter);
+    }
+  },duration);
+});
