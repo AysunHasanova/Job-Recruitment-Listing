24762f98d4128cfca8b37a87c110f45c3760236b
AysunHasanova <aysunhsnva@gmail.com>
2023-06-06 17:01:36 +0400

    login and signup bug fixed

--- assets/js/login.js
+++ assets/js/login.js
@@ -8,15 +8,18 @@ form.addEventListener("submit", function (e) {
 
   if (inputEmail && inputPassword) {
     let allUsers = JSON.parse(localStorage.getItem("usersData"));
-    let userObj = allUsers.find(
-      (item) =>
-        item.email === inputEmail.value && item.password === inputPassword.value
+    let userEmail = allUsers.find((item) => item.email === inputEmail.value);
+    let userPassword = allUsers.find(
+      (item) => item.password === inputPassword.value
     );
-    if (userObj) {
-      localStorage.setItem("userName", userObj.username);
+    console.log(userEmail);
+    if (userEmail && userPassword) {
+      // localStorage.setItem("userName", userObj.username);
       window.location = "index.html";
+    } else if (!userPassword && userEmail) {
+      alert("Password is wrong!")
     } else {
-      alert("No user you need register");
+      alert("No user found, you need sign up");
     }
   }
 });
--- assets/js/signup.js
+++ assets/js/signup.js
@@ -5,7 +5,7 @@ let inputEmail = document.querySelector("#inputEmail");
 let inputPassword = document.querySelector("#inputPassword");
 
 let userData = JSON.parse(localStorage.getItem("usersData")) ?? [];
-
+// console.log(userData);
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   let userObj = {
@@ -13,10 +13,21 @@ form.addEventListener("submit", function (e) {
     email: inputEmail.value,
     password: inputPassword.value,
   };
+  let found = false;
+  for (let i = 0; i < userData.length; i++) {
+    if (userObj.username === userData[i].username) {
+      found = true;
+      break;
+    }
+  }
+  // console.log(userData.includes(userObj));
+  if (found) {
+    alert("User is already");
+  } else {
+    userData.push(userObj);
 
-  userData.push(userObj);
+    localStorage.setItem("usersData", JSON.stringify(userData));
 
-  localStorage.setItem("usersData", JSON.stringify(userData));
-
-  window.location = "login.html";
+    window.location = "login.html";
+  }
 });
